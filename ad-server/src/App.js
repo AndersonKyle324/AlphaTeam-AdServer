670c3232f0306ae4a83101d821124e11b0993178
Jack Fales <46923410+jackfales@users.noreply.github.com>
2020-11-07 17:27:13 -0800

    Revert "Setup React project; moved learning sprint items to separate directory"
    This reverts commit 105a350fadde1a4341c8c4800a82990190c48ed8.

--- ad-server/src/App.js
+++
@@ -1,6 +0,0 @@
-import { withRouter } from 'react-router-dom';
-import Main from './components/Main/Main';
-
-const App = withRouter(Main);
-
-export default App;
--- ad-server/src/components/Management/ManagementPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default () => (
-    <div className="management-page">
-        <h1>Hello World!</h1>
-    </div>
-);
