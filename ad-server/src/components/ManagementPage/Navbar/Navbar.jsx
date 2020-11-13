cbcb18147baf19f8044a17d903eddc6488345ec4
Kyle Anderson <andersonkylea324@gmail.com>
2020-11-12 23:36:03 -0800

    basic ad bar, currently fuctions just print operation to console

--- ad-server/src/components/ManagementPage/Navbar/Navbar.jsx
+++ ad-server/src/components/ManagementPage/Navbar/Navbar.jsx
@@ -3,9 +3,29 @@ import './Navbar.css';
 
 export default () => {
     // JavaScript goes here
+    var openAdBoard = () => {console.log("open ad board")};
+    var openCampaigns = () => {console.log("open campaigns")};
+    var openReporting = () => {console.log("open reporting")};
+    var logout = () => {console.log("logout")};
 
     return (
         // JSX goes here
-        <h2>Navbar</h2>
+        <div className="navbar-wrapper">
+            <h2>Navbar</h2>
+            <div className="navbar-bar">
+                <button className="ad-board-button" onClick={() => openAdBoard()}>
+                    Ad Board
+                </button>
+                <button className="campaigns-button" onClick={() => openCampaigns()}>
+                    Campaigns
+                </button>
+                <button className="reporting-button" onClick={() => openReporting()}>
+                    Reporting
+                </button>
+                <button className="logout-button" onClick={() => logout()}>
+                    Logout
+                </button>
+            </div>    
+        </div>
     );
 };
