9dc739f1ce9680519e1b08d6782e084ceb502fcf
Kyle Anderson <andersonkylea324@gmail.com>
2020-11-13 00:33:43 -0800

    formatted forms so that functions can be inserted later

--- ad-server/src/components/ManagementPage/PublishModal/PublishModal.jsx
+++ ad-server/src/components/ManagementPage/PublishModal/PublishModal.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 //import './PublishModal.css';
 
 export default ({ show, closeModal }) => {
+    var handleUrlChange = (event) => {
+        //to do for handling url
+    }
+
+    var handleSubmit = (event) => {
+        //to do for handling form submit
+    }
+
     return (
         <div className="modal-wrapper"
             style={{
@@ -14,10 +22,18 @@ export default ({ show, closeModal }) => {
             </div>
             <div className="modal-body">
                 <form>
-                    <label>URL for ad to be displayed on
-                        <input type="text" name="url" />
-                        <input type="submit" value="Submit" />
+                    <label for="url">URL for ad to be displayed on
+                        <input 
+                            type="text" 
+                            name="url"
+                            onChange={ handleUrlChange } 
+                        />
                     </label>
+                    <input 
+                        type="submit" 
+                        value="Submit"
+                        onChange={ handleSubmit } 
+                    />
                 </form>
             </div>
         </div>
