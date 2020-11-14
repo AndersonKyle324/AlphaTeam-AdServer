21047ce6a34b1787089b49cfb05a11f26a1f8b5e
Justin Kim <justink875@gmail.com>
2020-11-13 16:32:53 -0800

    Reworked ad and campaign routes. Added PUT methods for ad and campaigns

--- server/index.js
+++ server/index.js
@@ -17,23 +17,63 @@ admin.initializeApp({
 // Initialize our DB
 const db = admin.firestore()
 
-//Get ads associated with ad campaign
+//Get the campaign information
 app.get('/campaign', async (req, res) => {
-    //front end sends a campaignID and we will return a json object
-    const docRef = db.collection('campaign').doc(req.body.campaignId);
-    res.json(docRef);
+    //We gather the whole campaign collection and return a list of campaign objects
+    const campaignRef = db.collection('campaign');
+    var campaignData = [];
+    await campaignRef.get()
+        .then(snapshot => {snapshot.forEach (doc => {
+            campaignData.push(doc.data());
+        })})
+        //catching an error if api request fails
+        .catch(err => {
+            console.log(err);
+        });
+
+    res.json(campaignData);
 })
 
 //Get ad information
 app.get('/ad', async (req, res) => {
-    //front end sends adID and we will return a json object
-    const docRef = db.collection('ads').doc(req.body.adId);
-    res.json(docRef);
-})
+    //We gather the whole ads collection and return a list of ad objects
+    const adRef = db.collection('ads');
+    var adData = [];
+    await adRef.get()
+        .then(snapshot => {snapshot.forEach (doc => {
+            adData.push(doc.data());
+        })})
+        //catching an error if api request fails
+        .catch(err => {
+            console.log(err);
+        });
 
-app.post('/ad/create', async (req, res) => {
-    const docRef = db.collection('ads')
-})
+    res.json(adData);
+});
+
+//Edits data with given resposne data and campaignID
+app.put('/campaign/edit', async (req, res) => {
+    //We get the campaignID from the request and replace the old data with the new.
+    const campaignDoc = db.collection('campaign').doc(req.body.campaignId);
+    await campaignDoc.set(req.body.campaignData)
+               .then(console.log(`Succesfully edited data for ${req.body.campaignId}`))
+               //Catches error in the case api request fails
+               .catch(err => {
+                   console.log(err);
+               });
+});
+
+//Edits data with given resposne data and adId
+app.put('/ad/edit', async (req, res) => {
+    //We get the adID from the request and replace the old data with the new.
+    const adDoc = db.collection('ad').doc(req.body.adId);
+    await adDoc.set(req.body.adData)
+               .then(console.log(`Succesfully edited data for ${req.body.adId}`))
+               //Catches error in the case api request fails
+               .catch(err => {
+                   console.log(err);
+               });
+});
 
 app.listen(port, () => {
     console.log(`Example listening on port ${port}`)
