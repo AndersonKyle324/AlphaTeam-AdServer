4115b980189a88db1be6a2f009328aecc7520622
Jack Fales <46923410+jackfales@users.noreply.github.com>
2020-11-16 20:44:43 -0800

    Added login page

---
+++ ad-server/src/components/ManagementPage/LoginPage/LoginPage.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Button, Form, Alert } from 'react-bootstrap';
+import './LoginPage.css';
+
+export default () => {
+    const [inputs, setInputs] = useState({
+        username: '',
+        password: '',
+        error: false
+    });
+
+    const handleSubmit = async () => {
+        if (inputs.username.trim() && inputs.password.trim()) {
+            const body = {
+                username: inputs.username,
+                password: inputs.password
+            };
+    
+            // send body to POST request for authentication
+            console.log(body);
+        } else {
+            setInputs({ ...inputs, error: true });
+        }
+    }
+
+    return (
+        <div className="login-page">
+            <h3>Login</h3>
+            <Form>
+                <Form.Group>
+                    <Form.Label className="font-weight-bold">Username</Form.Label>
+                    <Form.Control
+                        controlId="username"
+                        placeholder="Username"
+                        onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+                        value={inputs.username}
+                    />
+                    <Form.Label className="font-weight-bold">Password</Form.Label>
+                    <Form.Control
+                        controlId="password"
+                        placeholder="Password"
+                        onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+                        value={inputs.password}
+                    />
+                </Form.Group>
+                <Button className="primary" type="button" onClick={() => handleSubmit()}>Submit</Button>
+                { inputs.error ? <Alert variant="warning" className="p-2">Invalid Login Information</Alert> : null }
+            </Form>
+        </div>
+    );
+};
--- ad-server/src/components/ManagementPage/ManagementPage.jsx
+++ ad-server/src/components/ManagementPage/ManagementPage.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar/Navbar';
 import AddModal from './AddModal/AddModal';
 import AdTable from './AdTable/AdTable';
 import PublishModal from './PublishModal/PublishModal';
+import LoginPage from './LoginPage/LoginPage';
 
 export default () => {
     const [showPub, setShowPub] = useState(false);
@@ -11,6 +12,7 @@ export default () => {
     return (
     <div className="management-page">
         <h1>iFixit Ad Server</h1>
+        <LoginPage />
         <AdTable />
         <AddModal />
         <button className="open-publish-modal" onClick={() => setShowPub(true)}>
