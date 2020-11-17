d1980832e23e19ed85dcfc63f66299c618b032ee
AlexanderRodgers <alexedrodgers@gmail.com>
2020-11-16 22:02:04 -0800

    Added prettier and swagger

--- server/README.md
+++ server/README.md
@@ -1,7 +1,8 @@
 # Getting started with Firebase
+
 Go to [Firebase](https://firebase.google.com/) with whatever email you gave me and sign into the console. You should see a project that is something like IFixit Ad Server. The firebase DB that is used here will be how we store all of our user data. I've already done some authentication so you can just run
 
-* npm install
-* npm run dev
+-   npm install
+-   npm run dev
 
-Let me know if you have any questions.
+To run the linter run npm run format through the console
--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
-
 const express = require('express')
 const admin = require('firebase-admin')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.port || 3000
+
+const swaggerSettings = require('./swaggerSettings.json')
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+const specs = swaggerJsdoc(swaggerSettings)
+app.use('/docs', swaggerUi.serve)
+app.get(
+    '/docs',
+    swaggerUi.setup(specs, {
+        explorer: true,
+    })
+)
 
 // Load credentials from firebase
 const serviceAccount = require('./serviceAccountKey.json')
@@ -11,27 +23,37 @@ const serviceAccount = require('./serviceAccountKey.json')
 // Create the firebase connection
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'firebase-adminsdk-1vnku@ifixit-ad-server-edd98.iam.gserviceaccount.com'
+    databaseURL:
+        'firebase-adminsdk-1vnku@ifixit-ad-server-edd98.iam.gserviceaccount.com',
 })
 
 // Initialize our DB
 const db = admin.firestore()
 
 //Get the campaign information
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User management
+ */
 app.get('/campaign', async (req, res) => {
     //We gather the whole campaign collection and return a list of campaign objects
-    const campaignRef = db.collection('campaign');
-    var campaignData = [];
-    await campaignRef.get()
-        .then(snapshot => {snapshot.forEach (doc => {
-            campaignData.push(doc.data());
-        })})
+    const campaignRef = db.collection('campaign')
+    var campaignData = []
+    await campaignRef
+        .get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                campaignData.push(doc.data())
+            })
+        })
         //catching an error if api request fails
-        .catch(err => {
-            console.log(err);
-        });
+        .catch((err) => {
+            console.log(err)
+        })
 
-    res.json(campaignData);
+    res.json(campaignData)
 })
 
 /**
@@ -43,63 +65,91 @@ app.get('/campaign/:id', async (req, res) => {
         const docRef = db.collection('campaign').doc(campaignId)
         const campaignSnapshot = await docRef.get()
         if (!campaignSnapshot.exists) {
-            res.status(404).send({error: 'campaign does not exist', errorCode: 404 })
+            res.status(404).send({
+                error: 'campaign does not exist',
+                errorCode: 404,
+            })
         }
         const campaign = campaignSnapshot.data()
         res.status(200).send(campaign)
     } catch (e) {
-        res.status(500).send({error: 'Error retriving ad info', errorCode: 503 })
+        res.status(500).send({
+            error: 'Error retriving ad info',
+            errorCode: 503,
+        })
     }
 })
 
-//Get ad information
 app.get('/ad', async (req, res) => {
     //We gather the whole ads collection and return a list of ad objects
-    const adRef = db.collection('ads');
-    var adData = [];
-    await adRef.get()
-        .then(snapshot => {snapshot.forEach (doc => {
-            adData.push(doc.data());
-        })})
+    const adRef = db.collection('ads')
+    var adData = []
+    await adRef
+        .get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                adData.push(doc.data())
+            })
+        })
         //catching an error if api request fails
-        .catch(err => {
-            console.log(err);
-        });
+        .catch((err) => {
+            console.log(err)
+        })
 
-    res.json(adData);
-});
+    res.json(adData)
+})
 
 //Edits data with given resposne data and campaignID
 app.put('/campaign/edit', async (req, res) => {
     //We get the campaignID from the request and replace the old data with the new.
-    const campaignDoc = db.collection('campaign').doc(req.body.campaignId);
-    await campaignDoc.set(req.body.campaignData)
-               .then(console.log(`Succesfully edited data for ${req.body.campaignId}`))
-               //Catches error in the case api request fails
-               .catch(err => {
-                   console.log(err);
-               });
-});
+    const campaignDoc = db.collection('campaign').doc(req.body.campaignId)
+    await campaignDoc
+        .set(req.body.campaignData)
+        .then(console.log(`Succesfully edited data for ${req.body.campaignId}`))
+        //Catches error in the case api request fails
+        .catch((err) => {
+            console.log(err)
+        })
+})
 
 //Edits data with given resposne data and adId
 app.put('/ad/edit', async (req, res) => {
     //We get the adID from the request and replace the old data with the new.
-    const adDoc = db.collection('ad').doc(req.body.adId);
-    await adDoc.set(req.body.adData)
-               .then(console.log(`Succesfully edited data for ${req.body.adId}`))
-               //Catches error in the case api request fails
-               .catch(err => {
-                   console.log(err);
-               });
-});
+    const adDoc = db.collection('ad').doc(req.body.adId)
+    console.log(req.body)
+    await adDoc
+        .set(req.body.adData)
+        .then(console.log(`Succesfully edited data for ${req.body.adId}`))
+        //Catches error in the case api request fails
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.get('/ad/search', async (req, res) => {
+    try {
+        const title = req.query.title
+        const adRef = db.collection('ads')
+        const queryRef = await adRef.where('title', '==', title).get()
+        finalData = []
+        queryRef.forEach((doc) => {
+            finalData.push(doc.data())
+        })
+        res.status(200).send(finalData)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send({ error: 'Error retrieving search info' })
+    }
+})
+
 /**
  * Gets information for a specific add. If any flags are added to the URL it may also
  * update the impressions related to a given add.
  */
 app.get('/ad/:id', async (req, res) => {
     try {
-        const docId = req.params.id;
-        const docRef = db.collection('ad').doc(docId)
+        const docId = req.params.id
+        const docRef = db.collection('ads').doc(docId)
         const adSnapshot = await docRef.get()
         if (!adSnapshot.exists) {
             throw new Error('BROKEN')
@@ -113,9 +163,10 @@ app.get('/ad/:id', async (req, res) => {
             if (req.query.clicked) {
                 impressions.clicks += 1
             }
-            impressions.ctr = impressions.clicks !== 0
-                ? impressions.clicks / impressions.seen
-                : 0
+            impressions.ctr =
+                impressions.clicks !== 0
+                    ? impressions.clicks / impressions.seen
+                    : 0
             docRef.update({ impressions })
         }
         res.status(200).send(ad)
