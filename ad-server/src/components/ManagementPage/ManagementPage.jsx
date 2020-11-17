f2769d2ace97b1e7efe19e148b4a761ab5d4a535
Kyle Anderson <andersonkylea324@gmail.com>
2020-11-16 21:20:03 -0800

    navbar functionality now works, need to remove navbar file as it is all implemente din ManagementPage now

--- ad-server/src/components/ManagementPage/ManagementPage.jsx
+++ ad-server/src/components/ManagementPage/ManagementPage.jsx
@@ -1,29 +1,54 @@
 import React, { useState } from 'react';
-import Navbar from './Navbar/Navbar';
+// import Navbar from './Navbar/Navbar';
+import './ManagementPage.css'
 import AddModal from './AddModal/AddModal';
 import AdTable from './AdTable/AdTable';
 import PublishModal from './PublishModal/PublishModal';
 
 export default () => {
+    const [showAdTable, setShowAdTable] = useState(true);
+    const closeAdTable = () => setShowAdTable(false);
+
     const [showPub, setShowPub] = useState(false);
     const closePubModal = () => setShowPub(false);
 
     const [showAddModal, setShowAddModal] = useState(false);
-    const closeAddModal = () => setShowAddModal(false);
+    const closeAddModal = () => { 
+        setShowAddModal(false);
+        setShowAdTable(true);
+    }
+
+    const openAdTable = () => {
+        setShowAdTable(true);
+        closeAddModal();
+        closePubModal();
+    }
+
+    const openAddModal = () => {
+        setShowAddModal(true);
+        closeAdTable();
+        closePubModal();
+    }
 
     return (
         <div className="management-page">
             <h1>iFixit Ad Server</h1>
-            <AdTable />
-            <button className="open-add-modal" onClick={() => setShowAddModal(true)}>
-                Create new ad
-            </button>
-            <AddModal show={showAddModal} closeModal={closeAddModal}/>
-            <button className="open-publish-modal" onClick={() => setShowPub(true)}>
+            <div className="nav-bar">
+                <a className="open-ad-table" onClick={() => openAdTable()}>
+                    Ad Board
+                </a>
+                <a className="open-add-modal" onClick={() => openAddModal()}>
+                    Create New Ad
+                </a>
+                <AddModal show={showAddModal} closeModal={closeAddModal}/>
+
+                <AdTable show={showAdTable} closeModal={closeAdTable}/>
+            </div>
+
+            <a className="open-publish-modal" onClick={() => setShowPub(true)}>
                 Publish
-            </button>
+            </a>
             <PublishModal show={showPub} closeModal={closePubModal}/>
-            <Navbar />
         </div>
     );
 };
