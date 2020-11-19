d6af038e91170c5e8871fe931c9ce5f1851a7fb7
Jack Fales <46923410+jackfales@users.noreply.github.com>
2020-11-19 09:30:28 -0800

    Changed routing for login and management page

--- ad-server/src/components/Main/Main.jsx
+++ ad-server/src/components/Main/Main.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ManagementPage from '../ManagementPage/ManagementPage';
+import LoginPage from '../ManagementPage/LoginPage/LoginPage';
 
 export default () => (
     <div className="App">
         <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => (
-                  <ManagementPage />
-              )}
-            />
+            <Route exact path="/" render={() => <LoginPage />} />
+            <Route path="/management" render={() => <ManagementPage />} />
         </Switch>
     </div>
 );
