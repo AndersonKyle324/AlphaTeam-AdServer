63353a77fedb8b3245895bd8fb9b1ba88f2a5b59
Kyle Anderson <andersonkylea324@gmail.com>
2020-11-18 22:49:23 -0800

    tryed to understand using axios for campaginModal and PublishModal

--- ad-server/src/components/ManagementPage/PublishModal/PublishModal.jsx
+++ ad-server/src/components/ManagementPage/PublishModal/PublishModal.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 //import './PublishModal.css';
 
-export default ({ show, closeModal }) => {
+export default ({ show, closeModal, ad }) => {
+    var url = "";
+
     var handleUrlChange = (event) => {
         //to do for handling url
+        //url = event.target.value;
     }
 
     var handleSubmit = (event) => {
         //to do for handling form submit
+        if (url == ""){
+            alert("A url must be provided");
+        }
+        else{
+            //axios.get('ad/id', )
+            let adComponent = new Object();
+            //provide json with ad react component
+            //axios.get???('/ad/send??', data );
+        }
     }
 
     return (
