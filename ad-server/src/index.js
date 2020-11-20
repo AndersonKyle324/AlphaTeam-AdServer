d80a1fc444afbd5891ed5f04fbb2b3d5f01b17f7
Jack Fales <46923410+jackfales@users.noreply.github.com>
2020-11-20 14:26:30 -0800

    Properly loaded bootstrap styling

--- ad-server/src/index.js
+++ ad-server/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
