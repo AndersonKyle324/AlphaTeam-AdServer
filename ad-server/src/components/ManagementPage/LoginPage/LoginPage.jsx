b9928fbd64f43552a02a304b3044e0b48b68ed0d
Jack Fales <46923410+jackfales@users.noreply.github.com>
2020-11-20 14:52:48 -0800

    Styled login page

--- ad-server/src/components/ManagementPage/LoginPage/LoginPage.jsx
+++ ad-server/src/components/ManagementPage/LoginPage/LoginPage.jsx
@@ -24,28 +24,30 @@ export default () => {
     }
 
     return (
-        <div className="login-page">
-            <h3>Login</h3>
-            <Form>
-                <Form.Group>
-                    <Form.Label className="font-weight-bold">Username</Form.Label>
-                    <Form.Control
-                        controlId="username"
-                        placeholder="Username"
-                        onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
-                        value={inputs.username}
-                    />
-                    <Form.Label className="font-weight-bold">Password</Form.Label>
-                    <Form.Control
-                        controlId="password"
-                        placeholder="Password"
-                        onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-                        value={inputs.password}
-                    />
-                </Form.Group>
-                <Button className="primary" type="button" onClick={() => handleSubmit()}>Submit</Button>
-                { inputs.error ? <Alert variant="warning" className="p-2">Invalid Login Information</Alert> : null }
-            </Form>
+        <div className="ui-container">
+            <div className="login-page">
+                <h3>iFixit Ad Server Login</h3>
+                <Form>
+                    <Form.Group>
+                        <Form.Label className="font-weight-bold">Username</Form.Label>
+                        <Form.Control
+                            controlId="username"
+                            placeholder="Username"
+                            onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+                            value={inputs.username}
+                        />
+                        <Form.Label className="font-weight-bold">Password</Form.Label>
+                        <Form.Control
+                            controlId="password"
+                            placeholder="Password"
+                            onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+                            value={inputs.password}
+                        />
+                    </Form.Group>
+                    <Button className="primary" type="button" onClick={() => handleSubmit()}>Submit</Button>
+                    { inputs.error ? <Alert variant="warning" className="p-2">Invalid Login Information</Alert> : null }
+                </Form>
+            </div>
         </div>
     );
 };
