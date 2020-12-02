23e631ceb74c263abd1a9443e40bac0916e7623c
Takumi 荒井 <takumiarai@takumis-mbp.home>
2020-12-02 15:30:39 -0800

    AddModal AdPreview functioning

--- ad-server/src/components/ManagementPage/AddModal/AddModal.jsx
+++ ad-server/src/components/ManagementPage/AddModal/AddModal.jsx
@@ -4,34 +4,94 @@ import "./AddModal.css";
 
 export default (props) => {
   const [ad, setAd] = useState({
-    name: "",
-    campaign: "",
-    size: "",
-    imageFile: null,
-    altText: "",
-    title: "",
-    subtitle: "",
-    buttonAlign: null,
-    buttonText: "",
-    buttonUrl: "",
-    placeholder: "",
-  });
+    name       : "",
+    campaign   : "",
+    size       : "",
+    altText    : "",
+    title      : "",
+    subtitle   : "",
+    buttonAlign: "",
+    buttonText : "",
+    buttonUrl  : "",
+    imageFile  : false,
+    src        : false    // for preview
+  })
 
-  const handleInputChange = (e) => {};
-
-  const handleFileChange = () => {};
+  const handleInputChange = (e) => {}
 
   // Create an object of formData and post it to the server
-  const handFileUpload = () => {};
+  const handleImageUpload = (e) => {
+    var imageFile = e.target.files[0];
+    var src = URL.createObjectURL(imageFile);
+
+    setAd({...ad, imageFile: imageFile, src: src});
+  }
+
+  const renderPreview = () => {
+    if(ad.src) {
+      if(ad.buttonAlign === "Left") {
+        return(
+          <div className="preview" style={{textAlign: "left", backgroundImage: `url(${ad.src})`}}>
+            <div className="previewText">
+              <h3>{ad.title}</h3>
+              <h5>{ad.subtitle}</h5>
+            </div>
+            <div className="fakeButtonLeft">
+              <p>{ad.buttonText}</p>
+            </div>
+          </div>
+        );
+      }
+      else if (ad.buttonAlign === "Center") {
+        return(
+          <div className="preview" style={{backgroundImage: `url(${ad.src})`}}>
+            <div className="previewText">
+              <h3>{ad.title}</h3>
+              <h5>{ad.subtitle}</h5>
+            </div>
+            <div className="fakeButton">
+              <p>{ad.buttonText}</p>
+            </div>
+          </div>
+        );
+      }
+      else {
+        return(<p>Select Alignment</p>);
+      }
+    }
+    else{
+      return(
+        <p>Upload Background Image</p>
+      );
+    }
+  }
 
   const handleSubmit = () => {
     console.log(ad.name);
     if (ad.name == "") {
       alert("Provide an Ad Name");
     } else {
-      let data = new Object();
-      data.adId = ad.name;
+    const data = {
+      adId       : ad.name,
+      adName     : ad.name,
+      campaign   : ad.campaign,
+      size       : ad.size,
+      title      : ad.title,
+      subtitle   : ad.subtitle,
+      buttonAlign: ad.buttonAlign,
+      buttonText : ad.buttonText,
+      url        : ad.buttonUrl,
+      altText    : ad.altText,
+      imageFile  : null,
+      statistics : {
+        clicks   : 0,
+        seen     : 0,
+        ctr      : 0
+      }
     }
+    console.log(data);
+    //post ad data to server
+  }
   };
 
   return (
@@ -44,79 +104,74 @@ export default (props) => {
         <Form>
           <Row>
             <Col sm={4}>
-              <Form.Group controlId="ad.name">
-                <Form.Label>Ad Name</Form.Label>
-                <Form.Control placeholder="Enter ad name" />
-              </Form.Group>
-              <Form.Group controlId="ad.size">
-                <Form.Label>Ad Size</Form.Label>
-                <Form.Control as="select">
+              <Form.Label>Ad Name</Form.Label>
+              <Form.Control 
+                placeholder="Enter ad name"  
+                onChange={ (e) => setAd({...ad, name: e.target.value }) } 
+              />
+              <Form.Label>Ad Size</Form.Label>
+                <Form.Control as="select"
+                  onChange={ (e) => setAd({...ad, size: e.target.value }) } 
+                  >
                   <option>Select Ad Size</option>
-                  <option>Large: 961px x 200px</option>
                   <option>Small: 400px x 200px</option>
+                  <option>Large: 961px x 200px</option>
+
                 </Form.Control>
-              </Form.Group>
-              <Form.Group controlId="ad.campaign">
-                <Form.Label>Campaign</Form.Label>
-                <Form.Control as="select">
-                  <option>Select Campaign</option>
-                  <option>Repair Kit Sale</option>
-                  <option>Nintendo Switch</option>
-                </Form.Control>
-              </Form.Group>
-              <Form.Group>
-                <Form.File
+              <Form.Label>Campaign</Form.Label>
+              <Form.Control as="select"
+                onChange={ (e) => setAd({...ad, campaign: e.target.value }) } 
+                >
+                <option>Select Campaign</option>
+                <option>Repair Kit Sale</option>
+                <option>Nintendo Switch</option>
+              </Form.Control>
+              <Form.File
                   id="ad.imageFile"
                   label="Upload background image..."
+                  onChange={(e) => handleImageUpload(e)}
                 />
-              </Form.Group>
-              <Form.Group controlId="ad.altText">
-                <Form.Label>Alt text for image</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={2}
-                  placeholder="Describe your image for screen readers..."
-                />
-              </Form.Group>
+              <Form.Label>Alt text for image</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={4}
+                placeholder="Describe your image for screen readers..."
+                onChange={ (e) => setAd({...ad, altText: e.target.value }) } 
+              />
             </Col>
             <Col sm={8}>
-              <Form.Group controlId="ad.placeholder">
-                <Form.Label>Placeholder for preview</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={8}
-                  placeholder="This is a placeholder"
-                />
-              </Form.Group>
+              <div class="preview">
+                {renderPreview()}
+              </div>
               <Row>
                 <Col sm={6}>
-                  <Form.Group controlId="ad.title">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control placeholder="Enter Title" />
-                  </Form.Group>
-                  <Form.Group controlId="ad.subtitle">
-                    <Form.Label>Subtitle</Form.Label>
-                    <Form.Control placeholder="Enter Subtitle" />
-                  </Form.Group>
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control 
+                    onChange={ (e) => setAd({...ad, title: e.target.value }) } 
+                  />
+                  <Form.Label>Subtitle</Form.Label>
+                  <Form.Control 
+                    onChange={ (e) => setAd({...ad, subtitle: e.target.value }) } 
+                  />
                 </Col>
                 <Col sm={6}>
-                  <Form.Group controlId="ad.buttonAlign">
-                    <Form.Label>Button Alignment</Form.Label>
-                    <Form.Control as="select">
-                      <option>Select Button Alignment</option>
-                      <option>Left</option>
-                      <option>Center</option>
-                      <option>Right</option>
-                    </Form.Control>
-                  </Form.Group>
-                  <Form.Group controlId="ad.buttonText">
-                    <Form.Label>Button Text</Form.Label>
-                    <Form.Control placeholder="Enter Button Text" />
-                  </Form.Group>
-                  <Form.Group controlId="ad.buttonUrl">
-                    <Form.Label>Button Click Destination</Form.Label>
-                    <Form.Control placeholder="http://" />
-                  </Form.Group>
+                  <Form.Label>Button Alignment</Form.Label>
+                  <Form.Control as="select"
+                    onChange={ (e) => setAd({...ad, buttonAlign: e.target.value }) } 
+                    >
+                    <option>Select Button Alignment</option>
+                    <option>Left</option>
+                    <option>Center</option>
+                  </Form.Control>
+                  <Form.Label>Button Text</Form.Label>
+                  <Form.Control 
+                     onChange={ (e) => setAd({...ad, buttonText: e.target.value }) } 
+                  />
+                  <Form.Label>Button Click Destination</Form.Label>
+                  <Form.Control 
+                    placeholder="http://" 
+                    onChange={ (e) => setAd({...ad, buttonUrl: e.target.value }) } 
+                  />
                 </Col>
               </Row>
             </Col>
