7497ea1a2fa4b7fb96f99d0ba9dbc5df3281c218
Jack Fales <46923410+jackfales@users.noreply.github.com>
2020-12-04 17:18:34 -0800

    added component styling

---
+++ JackFales/component-styling/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ButtonPair from './components/ButtonPair';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ButtonPair />
+      </header>
+    </div>
+  );
+}
+
+export default App;
